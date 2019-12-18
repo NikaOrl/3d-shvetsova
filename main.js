bf798fdeca18368411078e1643d0de51647c77b6
Nika Orlova <veronika_orlova@epam.com>
2019-12-18 19:17:22 +0300

    add lab3

--- main.js
+++ main.js
@@ -19,10 +19,12 @@ function init() {
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMapEnabled = true;
+  renderer.shadowMap.type = THREE.BasicShadowMap;
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  renderer.toneMappingExposure = 1;
+
   document.body.appendChild(renderer.domElement);
-  // gamma
-  renderer.gammaOutput = true;
-  renderer.gammaFactor = 2.2; // approximate sRGB
   // scene
   scene = new THREE.Scene();
   // camera
@@ -41,7 +43,7 @@ function init() {
   // green cylinder
   var geometry = new THREE.CylinderGeometry(5, 5, 15, 32); // radiusTop?: number, radiusBottom?: number, height?: number, radiusSegments?: number, heightSegments?: number, openEnded?: boolean, thetaStart?: number, thetaLength?: number
   // geometry.translate(15, 15, 0);
-  var material = new THREE.MeshBasicMaterial({ color: 0xff19 });
+  var material = new THREE.MeshPhongMaterial({ color: 0xff19 });
   var cylinder = new THREE.Mesh(geometry, material);
   cylinder.rotation.x = Math.PI / 2;
   cylinder.rotation.z = Math.PI / 3;
@@ -50,7 +52,7 @@ function init() {
   // blue cylinder
   var geometry = new THREE.CylinderGeometry(3, 2, 5, 32); // radiusTop?: number, radiusBottom?: number, height?: number, radiusSegments?: number, heightSegments?: number, openEnded?: boolean, thetaStart?: number, thetaLength?: number
   geometry.translate(-4, 7.5, 2);
-  var material = new THREE.MeshBasicMaterial({ color: 0x1b9cbd });
+  var material = new THREE.MeshPhongMaterial({ color: 0x1b9cbd });
   var cylinder = new THREE.Mesh(geometry, material);
   // cylinder.rotation.x = Math.PI / 2;
   // cylinder.rotation.z = Math.PI / 3;
@@ -59,7 +61,7 @@ function init() {
   // red-blue cube
   var geometry_cube = new THREE.BoxGeometry(6.5, 13, 6.5);
   geometry_cube.translate(12.5, 1, 0);
-  var material = new THREE.MeshBasicMaterial({ color: 0xe02e3f });
+  var material = new THREE.MeshPhongMaterial({ color: 0xe02e3f });
   var cube = new THREE.Mesh(geometry_cube, material);
   cube.rotation.x = 0;
   cube.rotation.y = Math.PI / 6;
@@ -78,7 +80,7 @@ function init() {
     Math.PI
   );
   geometry.translate(0, -13, -5.5);
-  var material = new THREE.MeshBasicMaterial({ color: 0xfdcc30 });
+  var material = new THREE.MeshPhongMaterial({ color: 0xfdcc30 });
   var cylinder = new THREE.Mesh(geometry, material);
   cylinder.rotation.x = Math.PI / 2;
   cylinder.rotation.z = Math.PI / 3;
@@ -89,7 +91,7 @@ function init() {
   // green-blue-yellow cube
   var geometry_cube = new THREE.BoxGeometry(20, 13, 6.5);
   geometry_cube.translate(34, 1, 0);
-  var material = new THREE.MeshBasicMaterial({ color: 0xfdcc30 });
+  var material = new THREE.MeshPhongMaterial({ color: 0xfdcc30 });
   var cube = new THREE.Mesh(geometry_cube, material);
   cube.rotation.x = 0;
   cube.rotation.y = Math.PI / 6;
@@ -101,7 +103,7 @@ function init() {
   // green cube
   var geometry_cube = new THREE.BoxGeometry(20, 13, 6.5);
   geometry_cube.translate(62, 1, 0);
-  var material = new THREE.MeshBasicMaterial({ color: 0xff19 });
+  var material = new THREE.MeshPhongMaterial({ color: 0xff19 });
   var cube = new THREE.Mesh(geometry_cube, material);
   cube.rotation.x = 0;
   cube.rotation.y = Math.PI / 6;
@@ -120,7 +122,7 @@ function init() {
     Math.PI
   );
   geometry.translate(0, -62, -5.5);
-  var material = new THREE.MeshBasicMaterial({ color: 0x1b9cbd });
+  var material = new THREE.MeshPhongMaterial({ color: 0x1b9cbd });
   var cylinder = new THREE.Mesh(geometry, material);
   cylinder.rotation.x = Math.PI / 2;
   cylinder.rotation.z = Math.PI / 3;
@@ -130,7 +132,7 @@ function init() {
   var orbitControls = new OrbitControls(camera, renderer.domElement);
   orbitControls.addEventListener('change', render);
   orbitControls.minDistance = -100;
-  orbitControls.maxDistance = 100;
+  orbitControls.maxDistance = 150;
   orbitControls.enablePan = false;
 
   controls = new PointerLockControls(camera, document.body);
@@ -180,6 +182,30 @@ function init() {
   document.addEventListener('keydown', onKeyDown, false);
   document.addEventListener('keyup', onKeyUp, false);
 
+  // bottom
+  var geometry = new THREE.BoxGeometry(250, 1, 250, 2, 2, 2);
+  var material = new THREE.MeshLambertMaterial({ color: 0xfff });
+  var bottom = new THREE.Mesh(geometry, material);
+  bottom.position.set(0, -12.5, 0);
+  scene.add(bottom);
+
+  // Point light
+  const light = new THREE.PointLight(0xffffff, 2, 80);
+  light.position.set(0, 50, 0);
+  light.shadow.camera.near = 1;
+  light.shadow.camera.far = 62;
+  light.shadow.bias = -0.005; // reduces self-shadowing on double-sided objects
+  light.castShadow = true; // default false
+  scene.add(light);
+
+  // point light helper
+  var helper = new THREE.CameraHelper(light.shadow.camera);
+  scene.add(helper);
+
+  // Hole scene light
+  var ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
+  scene.add(ambientLight);
+
   render();
 }
 
@@ -207,7 +233,7 @@ function animate() {
 function addBase(xOption, wheelColor) {
   var geometry_cube = new THREE.BoxGeometry(25, 5, 10);
   geometry_cube.translate(5 + xOption, -7.5, 0);
-  var material = new THREE.MeshBasicMaterial({ color: 0xfbceae });
+  var material = new THREE.MeshPhongMaterial({ color: 0xfbceae });
   var cube = new THREE.Mesh(geometry_cube, material);
   cube.rotation.x = 0;
   cube.rotation.y = Math.PI / 6;
@@ -223,7 +249,7 @@ function addBase(xOption, wheelColor) {
 function addWheel(xOption, yOption, color) {
   var geometry = new THREE.CylinderGeometry(4.5, 4.5, 3, 32); // radiusTop?: number, radiusBottom?: number, height?: number, radiusSegments?: number, heightSegments?: number, openEnded?: boolean, thetaStart?: number, thetaLength?: number
   geometry.translate(-2 + xOption, yOption, 7.5);
-  var material = new THREE.MeshBasicMaterial({ color: color });
+  var material = new THREE.MeshPhongMaterial({ color: color });
   var cylinder = new THREE.Mesh(geometry, material);
   cylinder.rotation.x = Math.PI / 2;
   cylinder.rotation.z = -Math.PI / 6;
